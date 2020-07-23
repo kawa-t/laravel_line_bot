74e3db1b766bd7a21d8e98bab8629dfba7a81ba9
kata-n <kata.kawa813@gmail.com>
2020-07-23 14:42:36 +0900

    api設定修正

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,3 @@
 <?php
-
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['middleware' => ['api']], function () {
-  Route::get('get_access', 'GettingController@gettoeken');
-});
-
-Route::group(['namespace' => 'Api'], function() {
-  // LineからのWebhookを受信する
-  Route::post('/line/webhook', 'LineController@webhook')->name('line.webhook');
-});
+//LINE bot通信
+Route::post('/parrot', 'LineBotController@parrot');
